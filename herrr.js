78ae708daf0a791b261bc3a558b87c438f822f7f
arcelonferareza <167281188+arcelonferareza@users.noreply.github.com>
2024-07-17 12:19:16 +0800

    herrr.js

---
+++ herrr.js
@@ -0,0 +1,18 @@
+function showLetter() {
+    const letterContent = `
+        My Dearest baby,
+
+        From the moment our eyes met, I knew my life would never be the same. Your smile lights up my world, and your laughter is the sweetest melody I've ever heard. Every day with you is a beautiful adventure, and I am so grateful to have you by my side.
+
+        I love you more than words can say, and I look forward to creating countless memories with you. You are my heart, my soul, my everything.
+
+        I'm sorry for what I did to you honestly, if our arguments hurt you, it hurts me more. Thank you for coming into my life, I will never leave you
+
+        Your baby,
+        Arcel
+    `;
+
+    document.getElementById('letter-content').innerHTML = letterContent;
+    document.getElementById('letter-content').style.display = 'block';
+    document.getElementById('open-letter-btn').style.display = 'none';
+}
