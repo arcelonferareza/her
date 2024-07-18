d296292f8bc48b46532281a976cf91f551295e74
arcelonferareza <167281188+arcelonferareza@users.noreply.github.com>
2024-07-18 21:26:14 +0800

    herrr.js

--- herrr.js
+++ herrr.js
@@ -2,9 +2,11 @@ function showLetter() {
     const letterContent = `
         Hi baby hannah! I just want you to remember that I always love you, kahit nag tatampo or badtrip ka saakin. I've been courting you for 4 months now, and I'm happy. I'll 
 
-        court you every day even when the day comes that we're officially together. Thank you for not giving up on me, even though I sometimes overthink a lot. I miss you 
+        court you every day even when the day comes that we're officially together. Thank you for not giving up on me, even though I sometimes overthink a lot. 
+        
+        I always enjoy talking with you, laughing with you, and kissing you. I already treat you as my girlfriend, my fiancée, and my wife.
 
-        baby, ikaw lagi iniisip ko umaga hanggang gabi. Ikaw ang babaeng nagpatibok ng puso ko.  I love you more than words can say, and I look forward to creating countless 
+        I miss you baby, ikaw lagi iniisip ko umaga hanggang gabi. Ikaw ang babaeng nagpatibok ng puso ko.  I love you more than words can say, and I look forward to creating countless 
 
         memories with you. You are my heart, my soul, my everything.
 
